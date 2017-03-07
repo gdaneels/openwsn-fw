6d0ef1fe076757e2cbf9eed156dc97e6d4a09dad
Bruno Van de Velde <bruno@texus.me>
2017-03-07 18:49:09 +0100

    FW-493. Replaced radio register settings with values that seem to improve communication stability. This seems to seriously reduce the occurcance of crc errors and the weird issue where invalid sized packets were transmitted.

--- bsp/chips/cc1200-OpenMote/cc1200_conf.c
+++ bsp/chips/cc1200-OpenMote/cc1200_conf.c
@@ -26,7 +26,7 @@
 #define RF_CFG_MIN_TXPOWER              ( 0 )
 
 /* The maximum output power in dBm */
-#define RF_CFG_MAX_TXPOWER              ( 0 )
+#define RF_CFG_MAX_TXPOWER              ( 14 )
 
 /* The carrier sense level used for CCA in dBm */
 #define RF_CFG_CCA_THRESHOLD            ( -91 )
@@ -35,13 +35,13 @@
 
 /**
  * CC1200 configuration for IEEE 802.15.4
- * Modulation format = 2-GFSK
+ * Modulation format = 2-FSK
  * Whitening = false
  * Packet length = 255
  * Packet length mode = Variable
  * Packet bit length = 0
  * Symbol rate = 250
- * Deviation = 199.584961
+ * Deviation = 124.816895
  * Carrier frequency = 867.999878
  * Device address = 0
  * Manchester enable = false
@@ -51,34 +51,31 @@
  */
 static const cc1200_register_settings_t cc1200_register_settings [] = {
   {CC1200_IOCFG2,            0x06},
-  {CC1200_SYNC3,             0x6E},
-  {CC1200_SYNC2,             0x4E},
-  {CC1200_SYNC1,             0x90},
-  {CC1200_SYNC0,             0x4E},
-  {CC1200_SYNC_CFG1,         0xE5},
-  {CC1200_SYNC_CFG0,         0x23},
-  {CC1200_DEVIATION_M,       0x47},
-  {CC1200_MODCFG_DEV_E,      0x0E},
-  {CC1200_DCFILT_CFG,        0x1E},
-  {CC1200_PREAMBLE_CFG1,     0x19},
-  {CC1200_PREAMBLE_CFG0,     0xBA},
-  {CC1200_IQIC,              0xC8},
-  {CC1200_CHAN_BW,           0x41},
+  {CC1200_SYNC_CFG1,         0xA8},
+  {CC1200_SYNC_CFG0,         0x13},
+  {CC1200_DEVIATION_M,       0x99},
+  {CC1200_MODCFG_DEV_E,      0x05},
+  {CC1200_DCFILT_CFG,        0x26},
+  {CC1200_PREAMBLE_CFG0,     0x8A},
+  {CC1200_IQIC,              0x00},
+  {CC1200_CHAN_BW,           0x02},
   {CC1200_MDMCFG1,           0x42},
   {CC1200_MDMCFG0,           0x05},
   {CC1200_SYMBOL_RATE2,      0xB9},
   {CC1200_SYMBOL_RATE1,      0x99},
   {CC1200_SYMBOL_RATE0,      0x9A},
-  {CC1200_AGC_REF,           0x30},
-  {CC1200_AGC_CS_THR,        0xF8},
-  {CC1200_AGC_CFG2,          0x60},
-  {CC1200_AGC_CFG1,          0x12},
+  {CC1200_AGC_REF,           0x2F},
+  {CC1200_AGC_CS_THR,        0xEC},
+  {CC1200_AGC_CFG1,          0x16},
   {CC1200_AGC_CFG0,          0x84},
   {CC1200_FIFO_CFG,          0x00},
   {CC1200_FS_CFG,            0x12},
-  {CC1200_PKT_CFG2,          0x04},
+  {CC1200_PKT_CFG2,          0x00},
   {CC1200_PKT_CFG0,          0x20},
+  {CC1200_PA_CFG1,           0x6C},
   {CC1200_PKT_LEN,           0xFF},
+  {CC1200_IF_MIX_CFG,        0x18},
+  {CC1200_TOC_CFG,           0x03},
   {CC1200_MDMCFG2,           0x00},
   {CC1200_FREQ2,             0x56},
   {CC1200_FREQ1,             0xCC},
@@ -86,7 +83,7 @@ static const cc1200_register_settings_t cc1200_register_settings [] = {
   {CC1200_IF_ADC1,           0xEE},
   {CC1200_IF_ADC0,           0x10},
   {CC1200_FS_DIG1,           0x04},
-  {CC1200_FS_DIG0,           0xA3},
+  {CC1200_FS_DIG0,           0x50},
   {CC1200_FS_CAL1,           0x40},
   {CC1200_FS_CAL0,           0x0E},
   {CC1200_FS_DIVTWO,         0x03},
